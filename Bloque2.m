872c1920ec21f36835268d19ac30c5e4df3096a5
Rafael Caro <108595532+RafaCdlR@users.noreply.github.com>
2025-02-12 12:28:54 +0100

    Update Bloque2.m

--- Bloque2.m
+++ Bloque2.m
@@ -1,9 +1,8 @@
-trheshold = 0.99;
+threshold = 0.99;
 numeros = rgb2gray(imread("numeros.jpg"));
-% numrot = rgb2gray(imread("numerosRotados.jpg"));
 % Numero que se selecciona
 % select = 6;
 template = rgb2gray(imread(num2str(select) + ".jpg"));
 autocorr = normxcorr2(template, numeros);
-hits = numel(find(autocorr > trheshold));
+hits = numel(find(autocorr > threshold));
 disp("Numero de " + num2str(select) + " = " + hits);
